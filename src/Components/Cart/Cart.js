b60eb0fcbc6e89e72c3744e9c284aafd861358d9
shafik720 <shafikrasel5@gmail.com>
2022-10-25 16:22:35 +0600

    fdygdg

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Cart.css'
+const Cart = () => {
+    return (
+        <div>
+            <h2>Hello From Cart </h2>
+        </div>
+    );
+};
+
+export default Cart;
